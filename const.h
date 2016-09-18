dfe9653e7667af6c8c3b405cbde535b27c602158
mle <maximilian@eul.cc>
2016-09-18 13:56:38 +0100

    cs

--- const.h
+++ const.h
@@ -11,24 +11,24 @@
 #define EXIT_EMPTY	4
 #define EXIT_INTERNAL	10
 
-#define NAME_SEPARATOR "."
-#define OMIT_COLUMN "@"
+#define NAME_SEPARATOR	"."
+#define OMIT_COLUMN	"@"
 
 #ifndef PROGNAME
   #define PROGNAME "csv"
 #endif
 
-#define MAXLINELEN 16384
-#define MAXCOLUMNS 512
-#define MAXCOLDEFS 1024
-#define MAXCOLALIASES 40
+#define MAXLINELEN	16384
+#define MAXCOLUMNS	  512
+#define MAXCOLDEFS	 1024
+#define MAXCOLALIASES	   40
 
 
-#define M1 "[1m"  /* highlighted */
-#define Mu "[4m"  /* underlined */
-#define M0 "[0m"  /* normal */
+#define M1	"[1m"  /* highlighted */
+#define Mu	"[4m"  /* underlined */
+#define M0	"[0m"  /* normal */
 #define Myellow	"[33m"
-#define Mgreen 	"[32m"
+#define Mgreen	"[32m"
 
 
 #endif  // CONST_H
