d73a1f2081bc99e14e08784e357905ac128148f1
mle <maximilian@eul.cc>
2017-05-14 14:04:48 +0200

    test mixed quotes

--- test/02-test-quotes.sh
+++ test/02-test-quotes.sh
@@ -3,6 +3,7 @@
 
 fn1="col1"
 fn2="long \"col name"
+qv="backslash=\\\\ separator=; apos=' dq=\" eot"
 
 define expectedOutput <<-EOT
 	$fn1: first key
@@ -12,10 +13,26 @@ define expectedOutput <<-EOT
 	$fn2: quote=" eot
 	$RECORDSEP
 	$fn1: third key
-	$fn2: backslash=\\\\ separator=; eot
+	$fn2: $qv
 EOT
 
 assertCmdEq "$CSV -am < $SAMPLE/quotes.csv" "$expectedOutput" \
 	"csv did not handle \"-quoted values correctly!"
+
+
+define expectedOutput <<-EOT
+	$fn1: 1k
+	$fn2: first value
+	$RECORDSEP
+	$fn1: 2k
+	$fn2: $qv
+	$RECORDSEP
+	$fn1: 3k
+	$fn2: $qv
+
+EOT
+
+assertCmdEq "$CSV -am < $SAMPLE/mixed-quotes.csv" "$expectedOutput" \
+	"csv did not handle mixed-quoted values correctly!"
 
 success
